75a410432ba32f3007cc2176d4b307f5f32abd43
Sebastian Kasperczyk <sebastian.a.kasperczyk@gmail.com>
2024-01-22 21:45:02 +0100

    Add summary

--- EventAggregator/Program.cs
+++ EventAggregator/Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EventAggregator;
 
+int totalPublishedEvents = 0;
+int totalAggregatedEvents = 0;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("PRESS ANY KEY TO START...");
 _ = Console.ReadKey(false);
@@ -19,6 +22,7 @@ EventAggregator<RandomNumberPublishedEventArgs> aggregator = new()
 
 aggregator.Aggregated += (s, e) =>
 {
+    totalAggregatedEvents += e.AggregatedEvents.Count;
     Console.WriteLine(
         $"Aggregated events: {e.AggregatedEvents.Count}, " +
         $"avr: {e.AggregatedEvents.Average(e => e.Number)}, " +
@@ -28,6 +32,7 @@ aggregator.Aggregated += (s, e) =>
 
 publisher.EventPublished += (s, e) =>
 {
+    totalPublishedEvents++;
     Console.WriteLine($"Event published: {e.Number}, Thread={e.ThreadId}, Delay={e.PublishDelay}");
     aggregator.Consume(e);
 };
@@ -38,5 +43,19 @@ _ = Console.ReadKey(false);
 
 publisher.Stop();
 
+Console.WriteLine("PRESS ANY KEY TO START...");
+_ = Console.ReadKey(false);
+
+publisher.Start();
+
+Console.WriteLine("PRESS ANY KEY TO STOP...");
+_ = Console.ReadKey(false);
+
+publisher.Stop();
+
+Console.WriteLine("PRESS ANY KEY TO GET SUMMARY...");
+_ = Console.ReadKey(false);
+
+Console.WriteLine($"Total published evnets: {totalPublishedEvents}, total aggregated events: {totalAggregatedEvents}");
 Console.WriteLine("PRESS ANY KEY TO EXIT...");
 _ = Console.ReadKey(false);
